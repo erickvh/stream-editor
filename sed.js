43cc0f4be39446b6898c25d06c7362fc9c23175b
Erick Ventura <erick94111@gmail.com>
2020-05-07 15:14:12 -0600

    added validator to exactly to values and replace works without flags

--- sed.js
+++ sed.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const yargs = require('yargs')
   .boolean('n')
   .boolean('i')
-  .array('e')
+  // .array('e')
   .array('f')
   .describe(
     'n',
@@ -18,37 +18,53 @@ const yargs = require('yargs')
     'f',
     'Add the commands contained in the file script-file to the set of commands to be run while processing the input.'
   )
-  .help('h').argv;
+  .help('h')
+  .alias('h', 'help').argv;
+
 // own requires
 const validator = require('./src/validators');
 
-const raw_args = yargs._;
+// this variable store all the parameters in which it's not a option
+const rawArgs = yargs._;
+// validator needed for the minimun case needed
+validator.hasEnoughParams(rawArgs);
+// this variables will store pattern and filepath
+let pattern, filePath;
 
-const pattern = raw_args[0];
-const filePath = raw_args[1];
+[pattern, filePath] = rawArgs;
+
+// in case filepath is undefined, the first  raw parameter is the path
+if (!filePath) {
+  filePath = rawArgs[0];
+}
 
 const absolutePath = path.resolve(filePath);
 
-validator.isAValidPattern(pattern);
 validator.isAAccesibleFile(absolutePath);
 
+// start to readfile
 fs.readFile(absolutePath, (err, data) => {
   if (err) {
     console.log('Error while reading the file');
     return;
   }
-  let lines = data.toString().split('\n');
-  const splittedPattern = pattern.split('/');
-  const searchPattern = splittedPattern[1];
-  const stringToReplace = splittedPattern[2];
+  let contentText = data.toString();
+  let resultText = '';
+  // options: -i, -n
+  if (validator.hasTwoParams(rawArgs)) {
+    // first check if the pattern is correct
+    validator.isAValidPattern(pattern);
 
-  const searchRegexPattern = new RegExp(searchPattern);
+    const splittedPattern = pattern.split('/');
+    const searchPattern = splittedPattern[1];
+    const stringToReplace = splittedPattern[2];
+    const flag = splittedPattern[3];
 
-  lines = lines.map((line) =>
-    line.replace(searchRegexPattern, stringToReplace)
-  );
-
-  for (line of lines) {
-    console.log(line);
+    const searchRegexPattern = new RegExp(searchPattern);
+    resultText += contentText.replace(searchRegexPattern, stringToReplace);
+    console.log(resultText);
+  }
+  // options: -f,-e
+  else {
   }
 });
--- src/validators.js
+++ src/validators.js
@@ -2,6 +2,7 @@ const fs = require('fs').promises;
 
 const regexForPattern = /^s\/[a-zA-Z. ]+\/[a-zA-Z. ]+\/[g|p]?$/;
 
+// check if the pattern is correct for the regex given
 function isAValidPattern(pattern) {
   if (!regexForPattern.test(pattern)) {
     console.log(`pattern not valid ${pattern}`);
@@ -16,6 +17,7 @@ function isAValidPath(filepath) {
   }
 }
 
+// check if the file is readable
 async function isAAccesibleFile(absolutePath) {
   try {
     const data = await fs.access(absolutePath);
@@ -26,9 +28,65 @@ async function isAAccesibleFile(absolutePath) {
     return process.exit();
   }
 }
+// check if rawargs has at least one rawargs
+function hasAtLeastOneRawArgs(rawArgs) {
+  if (!(Array.isArray(rawArgs) && rawArgs.length >= 1)) {
+    console.log('any parameters to operate');
+    return process.exit();
+  }
+}
 
+// check if rawargs has many rawargs
+function hasManyRawArgs(rawArgs) {
+  if (!(Array.isArray(rawArgs) && rawArgs.length <= 2)) {
+    console.log('at least two parameters needed to operate');
+    return process.exit();
+  }
+}
+
+// check both validators into a single one
+function hasEnoughParams(rawArgs) {
+  hasAtLeastOneRawArgs(rawArgs);
+  hasManyRawArgs(rawArgs);
+}
+
+function isValidOneParam(yargs) {
+  let isValid = false;
+  // option n and i: doesn't need a single pattern and file
+  if (yargs.n || yargs.i) {
+    isValid = false;
+  }
+  return isValid;
+}
+
+function hasOptionIAndF(yargs) {
+  // first check if is valid for one param
+  if (isValidOneParam) {
+    // if it yargs containg both options is an invalid execution
+    if (yargs.e && yargs.f) {
+      const e = '-e';
+      const f = '-f';
+      console.log(`Both options ${e} ${f} are not valid together`);
+      return process.exit();
+    }
+  }
+}
+// when the params are two can be used a patter and a file
+function hasTwoParams(rawArgs) {
+  let hasTwoParams = false;
+  if (Array.isArray(rawArgs) && rawArgs.length == 2) {
+    hasTwoParams = true;
+  }
+  return hasTwoParams;
+}
 module.exports = {
   isAValidPath,
   isAValidPattern,
   isAAccesibleFile,
+  hasAtLeastOneRawArgs,
+  hasManyRawArgs,
+  isValidOneParam,
+  hasOptionIAndF,
+  hasEnoughParams,
+  hasTwoParams,
 };
